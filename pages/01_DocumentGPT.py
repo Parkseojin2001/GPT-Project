b035690848a220f4597bfac7365a8545e1001bde
Parkseojin2001 <seojin0510612@gmail.com>
2024-09-13 16:01:32 +0900

    7.8 Chain

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,23 +1,30 @@
-import time
+from langchain.prompts import ChatPromptTemplate
 import streamlit as st
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.storage import LocalFileStore
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📄",
 )
-    
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+
 @st.cache_data(show_spinner="Embedding files...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-        
+
     cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
 
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
@@ -33,15 +40,36 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
     if save:
         st.session_state["messages"].append({"message": message, "role": role})
-        
+
+
 def paint_history():
     for message in st.session_state["messages"]:
         send_message(message["message"], message["role"], False)
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            
+            Context: {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
 
 st.title("DocumentGPT")
 
@@ -63,16 +91,24 @@ with st.sidebar:
 
 if file:
     retriever = embed_file(file)
-    
+
     send_message("I'm ready! Ask away!", "ai", False)
     paint_history()
-    
+
     message = st.chat_input("Ask anything about your file...")
-    
+
     if message:
-        send_message(message, "human", True)
-        
+        send_message(message, "human")
+        chain = (
+            {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+
 else:
     st.session_state["messages"] = []
-    
-    
